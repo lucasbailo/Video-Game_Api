bb99926fc4aca1c57d5797deab7f2e508b6542e8
Lucas Bailo <lucasebailo@gmail.com>
2025-10-19 19:34:10 -0300

    create: creating get methods

---
+++ VideoGameApi/Controllers/VideoGameController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+        static private List<VideoGame> videoGames = new List<VideoGame>
+        {
+            new VideoGame
+            {
+                Id = 1,
+                Title = "The Legend of Zelda: Breath of the Wild",
+                Plataform = "Nintendo Switch",
+                Developer = "Nintendo EPD",
+                Publisher = "Nintendo"
+            },
+            new VideoGame
+            {
+                Id = 2,
+                Title = "God of War: Ragnarök",
+                Plataform = "PlayStation 5",
+                Developer = "Santa Monica Studio",
+                Publisher = "Sony Interactive Entertainment"
+            },
+            new VideoGame
+            {
+                Id = 3,
+                Title = "Elden Ring",
+                Plataform = "PC",
+                Developer = "FromSoftware",
+                Publisher = "Bandai Namco Entertainment"
+            },
+            new VideoGame
+            {
+                Id = 4,
+                Title = "Red Dead Redemption 2",
+                Plataform = "Xbox One",
+                Developer = "Rockstar Games",
+                Publisher = "Rockstar Games"
+            },
+            new VideoGame
+            {
+                Id = 5,
+                Title = "Super Mario Odyssey",
+                Plataform = "Nintendo Switch",
+                Developer = "Nintendo EPD",
+                Publisher = "Nintendo"
+            },
+            new VideoGame
+            {
+                Id = 6,
+                Title = "Halo Infinite",
+                Plataform = "Xbox Series X/S",
+                Developer = "343 Industries",
+                Publisher = "Xbox Game Studios"
+            },
+            new VideoGame
+            {
+                Id = 7,
+                Title = "Spider-Man 2",
+                Plataform = "PlayStation 5",
+                Developer = "Insomniac Games",
+                Publisher = "Sony Interactive Entertainment"
+            },
+            new VideoGame
+            {
+                Id = 8,
+                Title = "The Witcher 3: Wild Hunt",
+                Plataform = "PC",
+                Developer = "CD Projekt Red",
+                Publisher = "CD Projekt"
+            },
+            new VideoGame
+            {
+                Id = 9,
+                Title = "Minecraft",
+                Plataform = "Multi-platform",
+                Developer = "Mojang Studios",
+                Publisher = "Microsoft Studios"
+            },
+            new VideoGame
+            {
+                Id = 10,
+                Title = "Fortnite",
+                Plataform = "Multi-platform",
+                Developer = "Epic Games",
+                Publisher = "Epic Games"
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<VideoGame>> GetVideoGames()
+        {
+            return Ok(videoGames);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<VideoGame> GetVideoGameById(int id)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+            if (game is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(game);
+        }
+    }
+}
--- VideoGameApi/VideoGame.cs
+++ VideoGameApi/VideoGame.cs
@@ -2,5 +2,11 @@
 {
     public class VideoGame
     {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+        public string? Plataform { get; set; }
+        public string? Developer { get; set; }
+        public string? Publisher { get; set; }
     }
 }
